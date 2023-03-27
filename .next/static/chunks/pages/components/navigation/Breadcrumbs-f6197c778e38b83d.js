e02df151778bbb57ee7e09d07271f5c54cba0a05
adrianbrodzinski <adrbrodz@gmail.com>
2023-03-27 12:47:33 +0200

    update

--- .next/static/chunks/pages/components/navigation/Breadcrumbs-f6197c778e38b83d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[826],{9965:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/navigation/Breadcrumbs",function(){return u(6552)}])}},function(n){n.O(0,[774,888,179],(function(){return _=9965,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
