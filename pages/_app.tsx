d4141e45a6332cfe5bda714872c8c1ab35460a76
adrianbrodzinski <adrbrodz@gmail.com>
2024-01-25 12:58:59 +0100

    fix darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,14 @@ import Logo from "./components/navigation/Logo";
 import Breadcrumbs from "./components/navigation/Breadcrumbs";
 import user from "./master-data/edit-worker/[user]";
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
     const [loggedIn, setLoggedIn] = useState(false);
     const [userToken, setUserToken] = useState('');
 
     useEffect(() => {
+
         if (sessionStorage.getItem('company')) {
             setLoggedIn(true)
         } else {
@@ -23,7 +26,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         if (loggedIn == false) {
                 return (
-                    <div id="background" className="bg-accent-color-1 h-screen py-20 px-44">
+                    <div id="background" className="light bg-accent-color-1 h-screen py-20 px-44">
                         <div id="template" className="m-auto bg-white h-full min-w-[67rem] rounded-3xl pb-36 p-3">
                             <div className="mb-2">
                                 <Logo/>
